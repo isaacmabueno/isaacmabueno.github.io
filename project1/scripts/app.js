308398d0a1729c52bb738839bc0d51b8fc124b32
isaacmabueno <isaacmabueno@gmail.com>
2015-07-15 16:01:36 -0500

    wed 4:01 commit

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-  console.log("All resources are loaded");
+$('.logo').fadeIn(2000);
+$('').fadeIn(2000);
+
+
+
+$('').mouseenter(function(){
+  $(this).animate ({
+    height: '+=25px'
+  });
+$('').mouseleave(function(){
+  $(this).animate ({
+    height: '+=25px'
+  });
+
+})
+
+
+
+})
+
+
+
+
 
 });
