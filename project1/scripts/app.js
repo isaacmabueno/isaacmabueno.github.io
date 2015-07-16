76d451ed54cece9a4905cf75ea7d030614e0c6c4
isaacmabueno <isaacmabueno@gmail.com>
2015-07-15 22:56:01 -0500

    7/15 10:55pm

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,27 +1,32 @@
-$(document).ready(function(){
-
-$('.logo').fadeIn(2000);
-$('').fadeIn(2000);
+$(document).ready(function() {
 
 
 
-$('').mouseenter(function(){
-  $(this).animate ({
-    height: '+=25px'
-  });
-$('').mouseleave(function(){
-  $(this).animate ({
-    height: '+=25px'
+  $('.logo').fadeIn(4000);
+  $('.navbar').fadeIn(2000);
   });
 
-})
-
-
-
-})
-
-
-
-
-
-});
+//   $("#owl-demo").owlCarousel({
+//
+//       autoPlay: 3000, //Set AutoPlay to 3 seconds
+//
+//       items : 4,
+//       itemsDesktop : [1199,3],
+//       itemsDesktopSmall : [979,3]
+//
+//   });
+//
+// });
+//
+// $('').mouseenter(function(){
+//   $(this).animate ({
+//     height: '+=25px'
+//   });
+// $('').mouseleave(function(){
+//   $(this).animate ({
+//     height: '+=25px'
+//   });
+//
+// })
+// })
+// });
