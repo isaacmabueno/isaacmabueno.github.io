ce985ef7901773da69191893369a204e2960069c
isaacmabueno <isaacmabueno@gmail.com>
2015-07-16 18:24:50 -0500

    7-16 6:24

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -47,39 +47,6 @@ $('.logo').fadeIn(3000);
 
 
 
-// var app = app || {};
-// app.getRequest = {
-//   type: 'get',
-//   url: 'http://ga-chicago.github.io/project1/data.json'
-//   dataType: 'json',
-//   success: function(data) {
-//     console.log(data);
-//   },
-//   error: function(error) {
-//     console.log(error);
-//   }
-// };
-
-
-// var app = app
-// $.ajax(app.getRequest);
-
-
-
-
-
-//   $("#owl-demo").owlCarousel({
-//
-//       autoPlay: 3000, //Set AutoPlay to 3 seconds
-//
-//       items : 4,
-//       itemsDesktop : [1199,3],
-//       itemsDesktopSmall : [979,3]
-//
-//   });
-//
-// });
-//
 // $('').mouseenter(function(){
 //   $(this).animate ({
 //     height: '+=25px'
