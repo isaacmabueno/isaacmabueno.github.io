02ef543f193bdaf227e925a2f10f5d21406d70ae
isaacmabueno <isaacmabueno@gmail.com>
2015-07-16 15:53:32 -0500

    7-16 3:53PM

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,9 +1,52 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//
+//
+//   $('.logo').fadeIn(3000);
+//   });
+  //namespace 'app'
+  var app = app || {}
+  //fetch the weather
+  app.fetchWeather = function () {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=chicago,il", function(data){
+      console.log(data, "#weather");
+      //things to do with the data!
+      app.renderWeather(data,"#weather");
+    });
+    return true;
+
+
+  };
+  //render the weather
+  app.renderWeather = function(weatherData, selectorString) {
+    var template = weatherData.name + "," + weatherData.sys.country + " and the current weather is.. " + weatherData.weather[0].description;
+    $(selectorString).append(template);
+    return true;
+  };
+
+//when the document is ready
+$(document).ready(function(){
+  console.log("let's get ready to rumble!");
+  var weatherData = app.fetchWeather();
+
+$('.logo').fadeIn(3000);
+});
 
 
 
-  $('.logo').fadeIn(3000);
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var app = app || {};
 // app.getRequest = {
 //   type: 'get',
