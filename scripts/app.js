ec98e3102284e7cb7ebb5c5e8da9d07b068ba277
isaacmabueno <isaacmabueno@gmail.com>
2015-09-25 12:54:15 -0500

    better nav

--- scripts/app.js
+++ scripts/app.js
@@ -9,17 +9,17 @@ $(document).ready(function(){
   $('.materialboxed').materialbox();
     $(".button-collapse").sideNav();
 });
-$(document).ready(function() {
-    $(document).scroll(function () {
-        var scroll = $(this).scrollTop();
-        var topDist = $("#container").position();
-        if (scroll > topDist.top) {
-            $('nav').css({"position":"fixed","top":"0"});
-        } else {
-            $('nav').css({"position":"static","top":"auto"});
-        }
-    });
-});
+// $(document).ready(function() {
+//     $(document).scroll(function () {
+//         var scroll = $(this).scrollTop();
+//         var topDist = $("#container").position();
+//         if (scroll > topDist.top) {
+//             $('nav').css({"position":"fixed","top":"0"});
+//         } else {
+//             $('nav').css({"position":"static","top":"auto"});
+//         }
+//     });
+// });
 $(document).ready(function() {
   $('.talkingheads').hide();
 $('.talkingheads').delay(3000).fadeIn(3000);
